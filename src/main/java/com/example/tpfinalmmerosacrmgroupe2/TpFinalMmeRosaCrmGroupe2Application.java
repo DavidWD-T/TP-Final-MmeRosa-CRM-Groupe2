1f0f231bff7dbee60572f9a24bed2e419c3f3a6e
DavidWD-T <david_thelemaque@hotmail.fr>
2022-10-19 16:55:12 +0200

    init

---
+++ src/main/java/com/example/tpfinalmmerosacrmgroupe2/TpFinalMmeRosaCrmGroupe2Application.java
@@ -0,0 +1,13 @@
+package com.example.tpfinalmmerosacrmgroupe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalMmeRosaCrmGroupe2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpFinalMmeRosaCrmGroupe2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpfinalmmerosacrmgroupe2/TpFinalMmeRosaCrmGroupe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpfinalmmerosacrmgroupe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalMmeRosaCrmGroupe2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
